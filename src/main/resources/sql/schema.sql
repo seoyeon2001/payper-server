e501ff79eadd6aa8e9531cee1af96e4ab39bb3e3
Kim Eun Ji <118355919+Answl@users.noreply.github.com>
2025-07-28 12:10:42 +0900

    [REFACTOR #44]: schema.sql 파일 수정  (#49)
    * chore: gitignore 수정
    
    * refactor: schema.sql 수정

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -4,21 +4,22 @@
 
 SET FOREIGN_KEY_CHECKS = 0;
 
-DROP TABLE IF EXISTS benefit_grade;
 DROP TABLE IF EXISTS user_card_month_amount;
-DROP TABLE IF EXISTS annual_cost;
+DROP TABLE IF EXISTS benefit_grade;
+DROP TABLE IF EXISTS discount;
 DROP TABLE IF EXISTS benefit_limit;
 DROP TABLE IF EXISTS partner;
 DROP TABLE IF EXISTS category;
 DROP TABLE IF EXISTS benefit;
 DROP TABLE IF EXISTS grade;
+DROP TABLE IF EXISTS annual_cost;
 DROP TABLE IF EXISTS user_card;
 DROP TABLE IF EXISTS card;
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS company;
-DROP TABLE IF EXISTS discount;
 
 SET FOREIGN_KEY_CHECKS = 1;
+
 
 -- =========================
 -- CREATE TABLES START
@@ -28,14 +29,14 @@ SET FOREIGN_KEY_CHECKS = 1;
 
 -- 카드사
 CREATE TABLE `company` (
-                           `company_id` BIGINT NOT NULL AUTO_INCREMENT,
+                           `company_id` INT NOT NULL AUTO_INCREMENT,
                            `company_name` VARCHAR(10) NOT NULL,
                            PRIMARY KEY (`company_id`)
 );
 
 -- 사용자
 CREATE TABLE `user` (
-                        `user_id` BIGINT NOT NULL AUTO_INCREMENT,
+                        `user_id` INT NOT NULL AUTO_INCREMENT,
                         `oauth_provider` VARCHAR(255) NOT NULL,
                         `oauth_id` VARCHAR(255) NOT NULL,
                         `user_name` VARCHAR(10) NOT NULL,
@@ -44,8 +45,8 @@ CREATE TABLE `user` (
 
 -- 카드
 CREATE TABLE `card` (
-                        `card_id` BIGINT NOT NULL AUTO_INCREMENT,
-                        `company_id` BIGINT NOT NULL,
+                        `card_id` INT NOT NULL AUTO_INCREMENT,
+                        `company_id` INT NOT NULL,
                         `card_name` VARCHAR(100) NOT NULL,
                         `card_type` ENUM('CREDIT', 'CHECK') NOT NULL,
                         `card_image_url` VARCHAR(255) NOT NULL,
@@ -58,9 +59,9 @@ CREATE TABLE `card` (
 
 -- 사용자별 카드
 CREATE TABLE `user_card` (
-                             `user_card_id` BIGINT NOT NULL AUTO_INCREMENT,
-                             `user_id` BIGINT NOT NULL,
-                             `card_id` BIGINT NOT NULL,
+                             `user_card_id` INT NOT NULL AUTO_INCREMENT,
+                             `user_id` INT NOT NULL,
+                             `card_id` INT NOT NULL,
                              PRIMARY KEY (`user_card_id`),
                              CONSTRAINT `FK_user_TO_user_card` FOREIGN KEY (`user_id`)
                                  REFERENCES `user` (`user_id`)
@@ -72,11 +73,11 @@ CREATE TABLE `user_card` (
 
 -- 실적 등급
 CREATE TABLE `grade` (
-                         `grade_id` BIGINT NOT NULL AUTO_INCREMENT,
-                         `card_id` BIGINT NOT NULL,
-                         `start` INT NULL,
-                         `end` INT NULL,
-                         `total_discount` INT NULL,
+                         `grade_id` INT NOT NULL AUTO_INCREMENT,
+                         `card_id` INT NOT NULL,
+                         `start` BIGINT NULL,
+                         `end` BIGINT NULL,
+                         `total_discount` BIGINT NULL,
                          PRIMARY KEY (`grade_id`),
                          CONSTRAINT `FK_card_TO_grade` FOREIGN KEY (`card_id`)
                              REFERENCES `card` (`card_id`)
@@ -85,13 +86,13 @@ CREATE TABLE `grade` (
 
 -- 혜택
 CREATE TABLE `benefit` (
-                           `benefit_id` BIGINT NOT NULL AUTO_INCREMENT,
-                           `card_id` BIGINT NOT NULL,
+                           `benefit_id` INT NOT NULL AUTO_INCREMENT,
+                           `card_id` INT NOT NULL,
                            `benefit_title` VARCHAR(255) NOT NULL,
                            `benefit_summary` VARCHAR(255) NOT NULL,
                            `benefit_description` LONGTEXT NOT NULL,
                            `benefit_icon_url` VARCHAR(255) NOT NULL,
-                           `min_payment` INT NULL,
+                           `min_payment` BIGINT NULL,
                            PRIMARY KEY (`benefit_id`),
                            CONSTRAINT `FK_card_TO_benefit` FOREIGN KEY (`card_id`)
                                REFERENCES `card` (`card_id`)
@@ -100,10 +101,10 @@ CREATE TABLE `benefit` (
 
 -- 카테고리
 CREATE TABLE `category` (
-                            `category_id` BIGINT NOT NULL AUTO_INCREMENT,
-                            `benefit_id` BIGINT NOT NULL,
+                            `category_id` INT NOT NULL AUTO_INCREMENT,
+                            `benefit_id` INT NOT NULL,
                             `category_name` VARCHAR(10) NOT NULL,
-                            `pre_category_id` BIGINT NULL,
+                            `pre_category_id` INT NULL,
                             PRIMARY KEY (`category_id`),
                             CONSTRAINT `FK_benefit_TO_category` FOREIGN KEY (`benefit_id`)
                                 REFERENCES `benefit` (`benefit_id`)
@@ -115,9 +116,9 @@ CREATE TABLE `category` (
 
 -- 가맹점
 CREATE TABLE `partner` (
-                           `partner_id` BIGINT NOT NULL AUTO_INCREMENT,
-                           `category_id` BIGINT NOT NULL,
-                           `benefit_id` BIGINT NOT NULL,
+                           `partner_id` INT NOT NULL AUTO_INCREMENT,
+                           `category_id` INT NOT NULL,
+                           `benefit_id` INT NOT NULL,
                            `partner_name` VARCHAR(100) NOT NULL,
                            PRIMARY KEY (`partner_id`),
                            CONSTRAINT `FK_category_TO_partner` FOREIGN KEY (`category_id`)
@@ -130,12 +131,12 @@ CREATE TABLE `partner` (
 
 -- 혜택 제한
 CREATE TABLE `benefit_limit` (
-                                 `benefit_limit_id` BIGINT NOT NULL AUTO_INCREMENT,
-                                 `benefit_id` BIGINT NOT NULL,
-                                 `limit_count_per_day` INT NULL,
-                                 `limit_count_per_month` INT NULL,
-                                 `limit_count_per_year` INT NULL,
-                                 `limit_amount_per_pay` INT NULL,
+                                 `benefit_limit_id` INT NOT NULL AUTO_INCREMENT,
+                                 `benefit_id` INT NOT NULL,
+                                 `limit_count_per_day` BIGINT NULL,
+                                 `limit_count_per_month` BIGINT NULL,
+                                 `limit_count_per_year` BIGINT NULL,
+                                 `limit_amount_per_pay` BIGINT NULL,
                                  PRIMARY KEY (`benefit_limit_id`),
                                  CONSTRAINT `FK_benefit_TO_benefit_limit` FOREIGN KEY (`benefit_id`)
                                      REFERENCES `benefit` (`benefit_id`)
@@ -145,37 +146,23 @@ CREATE TABLE `benefit_limit` (
 
 -- 연회비
 CREATE TABLE `annual_cost` (
-                               `annual_cost_id` BIGINT NOT NULL AUTO_INCREMENT,
-                               `card_id` BIGINT NOT NULL,
+                               `annual_cost_id` INT NOT NULL AUTO_INCREMENT,
+                               `card_id` INT NOT NULL,
                                `brand_name` VARCHAR(10) NULL,
-                               `annual_fee` INT NULL,
-                               `co_annual_fee` INT NULL,
+                               `annual_fee` BIGINT NULL,
+                               `co_annual_fee` BIGINT NULL,
                                PRIMARY KEY (`annual_cost_id`),
                                CONSTRAINT `FK_card_TO_annual_cost` FOREIGN KEY (`card_id`)
                                    REFERENCES `card` (`card_id`)
                                    ON DELETE CASCADE ON UPDATE CASCADE
 );
 
--- 할인
-CREATE TABLE `discount` (
-                            `discount_id` BIGINT NOT NULL AUTO_INCREMENT,
-                            `type` ENUM('RATE', 'FIXED_AMOUNT') NOT NULL,
-                            `amount` INT NOT NULL,
-                            `limit_count` INT NULL,
-                            `limit_amount` INT NULL,
-                            PRIMARY KEY (`discount_id`)
-);
-
 -- 혜택별 실적별
 CREATE TABLE `benefit_grade` (
-                                 `benefit_grade_id` BIGINT NOT NULL AUTO_INCREMENT,
-                                 `discount_id` BIGINT NOT NULL,
-                                 `grade_id` BIGINT NOT NULL,
-                                 `benefit_id` BIGINT NOT NULL,
+                                 `benefit_grade_id` INT NOT NULL AUTO_INCREMENT,
+                                 `grade_id` INT NOT NULL,
+                                 `benefit_id` INT NOT NULL,
                                  PRIMARY KEY (`benefit_grade_id`),
-                                 CONSTRAINT `FK_discount_TO_benefit_grade` FOREIGN KEY (`discount_id`)
-                                     REFERENCES `discount` (`discount_id`)
-                                     ON DELETE CASCADE ON UPDATE CASCADE,
                                  CONSTRAINT `FK_grade_TO_benefit_grade` FOREIGN KEY (`grade_id`)
                                      REFERENCES `grade` (`grade_id`)
                                      ON DELETE CASCADE ON UPDATE CASCADE,
@@ -184,10 +171,25 @@ CREATE TABLE `benefit_grade` (
                                      ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+-- 할인
+CREATE TABLE `discount` (
+                            `discount_id` INT NOT NULL AUTO_INCREMENT,
+                            `benefit_grade_id` INT NOT NULL,
+                            `type` ENUM('RATE', 'FIXED_AMOUNT') NOT NULL,
+                            `amount` BIGINT NOT NULL,
+                            `limit_count` BIGINT NULL,
+                            `limit_amount` BIGINT NULL,
+                            PRIMARY KEY (`discount_id`),
+                            CONSTRAINT `FK_benefit_grade_TO_discount` FOREIGN KEY (`benefit_grade_id`)
+                                REFERENCES `benefit_grade` (`benefit_grade_id`)
+                                ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
 -- 사용자의 월별 카드 사용량
 CREATE TABLE `user_card_month_amount` (
-                                          `user_card_month_amount_id` BIGINT NOT NULL AUTO_INCREMENT,
-                                          `user_card_id` BIGINT NOT NULL,
+                                          `user_card_month_amount_id` INT NOT NULL AUTO_INCREMENT,
+                                          `user_card_id` INT NOT NULL,
                                           `month` CHAR(6) NOT NULL,
                                           `total_amount` BIGINT NOT NULL,
                                           PRIMARY KEY (`user_card_month_amount_id`),
