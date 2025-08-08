6f14fc7e8d065e7e46ad963a5396c04bcac93e32
Gyehyun Park <167384362+gyehyun-bak@users.noreply.github.com>
2025-08-08 16:05:16 +0900

    [SECURITY #254]: /api/crawl 엔드포인트 인가 허용 (#255)
    security: added /api/crawl/** in SecurityConfig

--- src/main/java/com/payper/global/security/config/SecurityConfig.java
+++ src/main/java/com/payper/global/security/config/SecurityConfig.java
@@ -71,7 +71,8 @@ public class SecurityConfig {
                                 "/docs/**",
                                 "/api/auth/**",
                                 "/ws/**",
-                                "/actuator/**"
+                                "/actuator/**",
+                                "/api/crawl/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
